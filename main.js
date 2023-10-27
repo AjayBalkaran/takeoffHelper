d04aff5bb8426450989fa75f5a84f99a0a4da85e
Ajay Balkaran <49107213+AjayBalkaran@users.noreply.github.com>
2023-10-27 10:36:54 -0400

    Enhance CSV data handling and filtering
    - Added functionality to filter data for "ABalkaran" and "BALL VALVE."
    - Implemented grouping and counting by "Subject," "Page Label," and "Label."
    - Improved code readability and structure.

--- main.js
+++ main.js
@@ -16,18 +16,41 @@ function handleFileSelect (event) {
             dynamicTyping: true,
             complete: function (results) {
                 const parsedData = results.data
-                console.log('Parsed data', parsedData)
+                // console.log('Parsed data', parsedData) 
 
-            // filter the data to only include amico pipeline employes mark up
-            const pipeEstOnlyData = parsedData.filter((item) => item.Author === "ABalkaran")
-                console.log('Pipe Estimator Mark up only', pipeEstOnlyData)
+                // filter the data to only include amico pipeline employes mark up
+                const pipeEstOnlyData = parsedData.filter((item) => item.Author === "ABalkaran");
+                console.log('Pipe Estimator Mark up only', pipeEstOnlyData);
 
-            // // Group and count by "Subject," "Page Label," and "Label"
-            // const groupedData = pipeEstOnlyData.reduce((acc, item) => {
+                // filter estimators mark up for all valves. 
+                const valveData = pipeEstOnlyData.filter((item) => item.Subject === "BALL VALVE");
+                console.log('Pipe est valves mark up:', valveData);
+                
 
-            // }
-            // )
+                // Group and count by "Subject," "Page Label," and "Label"
+                const groupedData = valveData.reduce((acc, item) => {
+                const {Subject, "Page Label": pageLabel, Label, Count} = item;
+                    // Create a unique key for each combination of "Subject," "Page Label," and "Label"
+                    const key = `${Subject}_${pageLabel}_${Label}`;
 
+                    //check if key exist if not creat it
+                    if (!acc[key]) {
+                        acc[key] = {
+                            Subject, 
+                            "Page Label": pageLabel, 
+                            Label, 
+                            Count: 0
+                        }
+                    }
+
+                    //add the count to the current item to the key 
+                    acc[key].Count += Count;
+                    return acc;
+                }, {});
+
+                // Convert the grouped data into an array
+                const groupedArray = Object.values(groupedData);
+                console.log("THIS IS FILTERD:", groupedArray);
             }
         });
     } else {
